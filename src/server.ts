6324cba203e45a5f74d5228023fc2b84c249bf5c
Fahmida0010 <fahmidaakter0010@gmail.com>
2026-07-15 17:18:47 +0600

    fix vercel server entry

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== "production") {
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
+
 });
+}
